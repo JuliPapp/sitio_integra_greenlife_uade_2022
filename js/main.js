97efc275a781d4a63f7033efbc17f4b0714575a8
JuliPapp <84880514+JuliPapp@users.noreply.github.com>
2022-10-04 01:31:08 -0300

    slider home listos

--- js/main.js
+++ js/main.js
@@ -8,13 +8,50 @@ function myFunction() {
     }
   }
 
+//slider1
   $(document).ready(function(){
-    $('.autoplay').slick({
+    $('.autoplay1').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2500,
-      speed: 500,
+      speed: 1000,
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      customPaging: function(slider, i) {
+        // this example would render "tabs" with titles
+        return '<span class="dot"></span>';
+      },
+    });
+  });
+
+  //slider2
+  $(document).ready(function(){
+    $('.autoplay2').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2800,
+      speed: 1200,
+      arrows: false,
+      dots: false,
+      adaptiveHeight: true,
+      customPaging: function(slider, i) {
+        // this example would render "tabs" with titles
+        return '<span class="dot"></span>';
+      },
+    });
+  });
+
+  //slider3
+  $(document).ready(function(){
+    $('.autoplay3').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      speed: 1500,
       arrows: false,
       dots: true,
       adaptiveHeight: true,
